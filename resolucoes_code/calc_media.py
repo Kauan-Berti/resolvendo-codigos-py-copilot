0b2201db3a7dc7493d5e62d1335288f687bc37c9
Kauan-Berti <111374188+Kauan-Berti@users.noreply.github.com>
2025-07-20 16:25:53 +0000

    calculo de media de notas

---
+++ resolucoes_code/calc_media.py
@@ -0,0 +1,5 @@
+#Descrição: Agora vamos calcular a média de três notas fornecidas na entrada do usuário. Uma dica é: Utilize operadores aritméticos para realizar o cálculo da média.
+prova1 = float(input("Digite a primeira nota da primeira Prova: "));
+prova2 = float(input("Digite a segunda nota da primeira Prova: "));
+media = (prova1 + prova2) / 2;
+print(f"Sua média é: {media}");
